af907fb6adf73bf7dc9f754ccd511e9869483726
xtinne <tinne.ringo@hotmail.com>
2016-08-24 15:58:39 +0200

    CouchDB
    Tonen van de bestellingen en opleveren van de bestelling, wel nog op
    eenzelfde pagina

--- Webtech2Oefn1/app/_attachments/scripts/main.js
+++ Webtech2Oefn1/app/_attachments/scripts/main.js
@@ -3,6 +3,7 @@ function createDoc(){
 	var hoeveelheid = $("#hoeveelheid").val();
 	var tafelNr = $("#tafelNr").val();
 	var opmerkingen = $("#opmerkingen").val();
+	var opgeleverd = 0;
 	var datum = new Date();
 	
 	var doc = {};
@@ -12,6 +13,7 @@ function createDoc(){
 	doc.tafelNr = tafelNr;
 	doc.opmerkingen = opmerkingen;
 	doc.datum = datum;
+	doc.opgeleverd = opgeleverd;
 	var json = JSON.stringify(doc);
 	console.log(json);
 	
@@ -41,12 +43,12 @@ function buildOutput(){
         success:		function(data){
         	
         	var arr = JSON.parse(data).rows;
-        	var htmlString = '<table>';
+        	var htmlString = '<table class="table table-hover"><tr><td>Gerecht</td><td>Hoeveelheid</td><td>Tafelnummer</td><td>Opmerkingen</td><td>Datum en uur van bestelling</td><td>Opgeleverd</td></tr>';
         	for(var i=0; i<arr.length; i++){
         		
         		if(arr[i].id.indexOf('_design') == -1){
         			var doc = arr[i].doc;
-        			htmlString += '<tr><td>' + doc.gerecht + '</td><td>' + doc.hoeveelheid + '</td><td>' + doc.tafelNr + '</td><td>' + doc.opmerkingen + '</td><td>' + doc.datum + '</td></tr>';
+        			htmlString += '<tr><td>' + doc.gerecht + '</td><td>' + doc.hoeveelheid + '</td><td>' + doc.tafelNr + '</td><td>' + doc.opmerkingen + '</td><td>' + doc.datum + '</td><td>' + '<button type="button" class="btn btn-success" onClick="editDoc(\'' + doc._id + '\',\'' + doc._rev + '\',\'' + doc.gerecht + '\',\'' + doc.hoeveelheid + '\',\'' + doc.tafelNr + '\',\'' + doc.opmerkingen + '\',\'' + doc.datum + '\',\'' + doc.opgeleverd + '\')">Opleveren</button>' + '</td></tr>';
         		}
         	}
         	htmlString += '</table>';
@@ -55,6 +57,69 @@ function buildOutput(){
         },
 		error: 			function(XMLHttpRequest, textStatus, errorThrown){ 
 			console.log(errorThrown); 
+		}
+	});
+}
+
+function editDoc(id, rev, gerecht, hoeveelheid, tafelNr, opmerkingen, datum, opgeleverd){
+	
+	$('#output').hide();
+	$('#edit').show();
+	
+	var html = '';
+
+	html += '<h3>Lever op</h3><table class="table table-hover">';
+	html += '<input type="hidden" id="_id" value="' + id + '"/>';
+	html += '<input type="hidden" id="_rev" value="' + rev + '"/>';
+	html += '<input type="hidden" id="gerecht" value ="' + gerecht + '"/>';
+	html += '<input type="hidden" id="hoeveelheid" value ="' + hoeveelheid + '"/>';
+	html += '<input type="hidden" id="tafelNr" value ="' + tafelNr + '"/>';
+	html += '<input type="hidden" id="gerecht" value ="' + opmerkingen + '"/>';
+	html += '<input type="hidden" id="gerecht" value ="' + datum + '"/>';
+	html += '<tr><td>Opgeleverd:</td><td><input id="opgeleverd2" type="checkbox" value="' + opgeleverd + '"/></td></tr>';
+	html += '<tr><td colspan="2" align="center"><button type="button" class="btn btn-primary" onClick="updateDoc()">Ok</button></td></tr>';
+	html += '</table>';
+	
+	$('#edit').html(html);
+}
+
+
+function updateDoc(){
+	
+	var id = $("#_id").val();
+	var rev = $("#_rev").val();
+	var gerecht = $("#gerecht").val();
+	var hoeveelheid = $("#hoeveelheid").val();
+	var tafelNr = $("#tafelNr").val();
+	var opmerkingen = $("#opmerkingen").val();
+	var datum = $("#datum").val();
+	var opgeleverd = $("#opgeleverd2").val();
+
+	var doc = {};
+
+	doc._id = id;
+	doc._rev = rev;
+	doc.gerecht = gerecht;
+	doc.hoeveelheid = hoeveelheid;
+	doc.tafelNr = tafelNr;
+	doc.opmerkingen = opmerkingen;
+	doc.datum = datum;
+	doc.opgeleverd = opgeleverd;
+	var json = JSON.stringify(doc);
+
+	$.ajax({
+		type : 'PUT',
+		url : '../../' + id,
+		data : json,
+		contentType : 'application/json',
+		async : true,
+		success : function(data){
+			$('#edit').hide();
+			$('#output').show();
+			buildOutput();
+		},
+		error : function(XMLHttpRequest, textStatus, errorThrown){
+			console.log(errorThrown);
 		}
 	});
 }
